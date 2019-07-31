984feae24e09f4d8ea39be7a172c7692b2bc5306
ameraz <ameraz@csumb.edu>
2019-07-31 01:32:28 -0400

    updated app.js so that it works with Heroku

--- app.js
+++ app.js
@@ -25,10 +25,10 @@ app.get("/tutorials", function(req, res){
 });
 
 //server listener
-app.listen("8081", "0.0.0.0", function(){
-  console.log("Express Server is Running...");
-});
-
-// app.listen(process.env.PORT, process.env.IP, function() {
-//   console.log("Running Express Server...");
+// app.listen("8081", "0.0.0.0", function(){
+//   console.log("Express Server is Running...");
 // });
+
+app.listen(process.env.PORT, process.env.IP, function() {
+  console.log("Running Express Server...");
+});
