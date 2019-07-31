de0dc68d78a7eef9204387d5b343fd073817ca5c
ameraz <ameraz@csumb.edu>
2019-07-31 01:31:38 -0400

    Redid HW1 using Express

---
+++ app.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const app = express();
+app.engine('html', require('ejs').renderFile);
+app.use(express.static("public"));
+
+const faker = require("faker");
+
+//root route
+app.get("/", function(req, res) {
+  //res.send("it works!");
+  res.render("index.html");
+});
+
+app.get("/references", function(req, res){
+  res.render("references.html");
+});
+
+app.get("/solutions", function(req, res){
+  var company = faker.company;
+  res.render("solutions.html", {"companyName":company.companyName(), "companyDescription": company.catchPhrase(), "companyImg": faker.image.business()});
+});
+
+app.get("/tutorials", function(req, res){
+  res.render("tutorials.html");
+});
+
+//server listener
+app.listen("8081", "0.0.0.0", function(){
+  console.log("Express Server is Running...");
+});
+
+// app.listen(process.env.PORT, process.env.IP, function() {
+//   console.log("Running Express Server...");
+// });
